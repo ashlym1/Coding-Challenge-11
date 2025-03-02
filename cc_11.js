d02140e29e837cb1ba0d9d5da41dfc2b08a9fc8b
ashlym1 <ashlym3@usf.edu>
2025-03-02 13:19:22 -0500

    Fixed typo In task 3 in listbooks method

--- cc_11.js
+++ cc_11.js
@@ -14,14 +14,14 @@
        this.copies +=quantity;
 }
  }      
-        // Test Cases : 
-            const book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 123456, 5);
-            console.log(book1.getDetails());
-            // Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 5"
-            
-            book1.updateCopies(-1);
-            console.log(book1.getDetails());
-            // Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 4"
+     // Test Cases : 
+     const book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 123456, 5);
+     console.log(book1.getDetails());
+     // Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 5"
+  
+     book1.updateCopies(-1);
+     console.log(book1.getDetails());
+      // Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 4"
 
  //* Task 2- Creating Borrower Class: A person borrowing a book and the following is to track the books. 
     class Borrower {
@@ -67,7 +67,7 @@
     listBooks() {
         console.log("List of Books in the Library : " ); // Mehtod to list all the books that are in the Library
         this.books.forEach(book=> { 
-            console.log(book,getDetails ()); // 
+            console.log(book.getDetails ()); // 
         }) ;
     }
     }
